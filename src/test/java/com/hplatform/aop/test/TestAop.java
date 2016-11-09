71e077be502150d49396e526d02196bf9abc41e4
javahao <353475081@qq.com>
2016-11-09 18:09:44 +0800

    修复无法junittest，测试

--- src/test/java/com/hplatform/aop/test/TestAop.java
+++ src/test/java/com/hplatform/aop/test/TestAop.java
@@ -5,9 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.hplatform.aop.excute.AspectExcute;
-
+@WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class) //指定测试用例的运行器 这里是指定了Junit4
 @ContextConfiguration(locations={"classpath:spring-config.xml"}) //指定Spring的配置文件 /为classpath下
 public class TestAop {
